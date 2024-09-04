ba3e64d9fea2176be76cbf1b0365f250263fb5a2
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2024-09-03 23:07:02 -0400

    Update serial.py

--- serial.py
+++ serial.py
@@ -5,12 +5,15 @@ port = list(list_ports.comports())
 for p in port:
     print(p.device)
 while True:
-    ser=serial.Serial('COM16',baudrate=9600,bytesize=8,parity='N',stopbits=1)
+    ser=serial.Serial('COM14',baudrate=9600,bytesize=8,parity='N',stopbits=1)
     #ser = serial.Serial('COM3', 9600)
     time.sleep(2)
-    ser.write(b'dato')
-    respuesta=ser.readline()
+    ser.write(b'1')
+    respuesta=ser.readline().decode('utf-8')
+    #respuesta=ser.readline().decode('ascii')
     print(respuesta)
+    time.sleep(5)
+    ser.write(b'2')
     ser.close()
     print("dentro del bucle")
 
